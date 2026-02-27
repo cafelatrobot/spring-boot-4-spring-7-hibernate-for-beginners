2d64046616d1771037bd24b3bc21262b2db18f52
gregpent <gregpentium@gmail.com>
2026-02-27 10:52:34 +0000

    updates

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -28,6 +29,19 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
 
         String result = "Yo, " + theName;
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName,
+                                          Model model){
+
+        theName = theName.toUpperCase();
+
+        String result = "hey.... " + theName;
 
         model.addAttribute("message", result);
 
